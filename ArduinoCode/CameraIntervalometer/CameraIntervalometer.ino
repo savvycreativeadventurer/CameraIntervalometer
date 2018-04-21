f99e835b3fad6b5905f04abd8d52b408289dd1ba
Ben Rabe <benrabe.sa@gmail.com>
2018-04-21 18:59:48 +0200

    Arduino Sketch Started
    Added Basic Steps

---
+++ ArduinoCode/CameraIntervalometer/CameraIntervalometer.ino
@@ -0,0 +1,23 @@
+
+
+int CameraFocusPin=9;
+int CameraShutterPin=10;
+int CameraShutterDelay=250;
+int CameraShutterTriggerDelay=250;
+int CameraShutterCount=10;
+
+
+
+void setup() {
+  pinMode(CameraShutterPin,OUTPUT);
+  pinMode(CameraFocusPin,OUTPUT);
+}
+
+void loop() {
+  for (int j=0; j<=CameraShutterCount; j=j+1){
+  digitalWrite(CameraShutterPin,HIGH);
+  delay (CameraShutterDelay);
+  digitalWrite(CameraShutterPin,LOW);
+  delay (CameraShutterTriggerDelay);
+  }
+}
